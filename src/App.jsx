29d3f9e9506c79350c5b3063f5798cf5c4417501
Arthur16800 <arthur08coutinho@gmail.com>
2025-08-18 14:43:29 -0300

    Adição do componente header ao App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
 import Home from "./pages/Home";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>   
-        <Route path="/login" element={<Login />}/>   
-        <Route path="/cadastro" element={<Cadastro />}/>   
+        <Route
+          path="/"
+          element={
+            <Header>
+              <Home />
+            </Header>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
     </Router>
   );
